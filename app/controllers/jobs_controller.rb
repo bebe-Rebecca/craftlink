84b558237d694c90f14a2682dd6089574660556c
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-04-23 18:54:31 +0900

    記事投稿機能の実装

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,27 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+  def index
+    @jobs = Job.all.order('created_at DESC')
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:job_title, :workshop_name, :description, :origin_to_prefecture_id, :image).merge(user_id: current_user.id)
+  end
+  
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,12 @@
 class Job < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :origin_to_prefecture
+
+  validates :job_title,               presence: true
+  validates :workshop_name,           presence: true
+  validates :description,             presence: true
+  validates :image,                   presence: true                  
+  validates :origin_to_prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
 end
