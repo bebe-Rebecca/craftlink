5bbb1033cf3468906ba8ce467d37d99b414b16c1
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-04-23 18:56:58 +0900

    記事投稿機能のモデルテスト

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :job do
-    
+    job_title { Faker::Name.name }
+    workshop_name { Faker::Name.name }
+    origin_to_prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    description { Faker::Lorem.sentence }
+
+    association :user
+
+    after(:build) do |job|
+      job.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @job = FactoryBot.build(:job)
+  end
+
+  describe '投稿' do
+    context '投稿できる場合' do
+      it '全項目の値が存在すれば投稿できる' do
+        expect(@job).to be_valid
+      end
+    end
+    context '投稿できない場合' do
+      it '画像が添付されていないと投稿できない' do
+        @job.image = nil
+        @job.valid?
+        expect(@job.errors.full_messages).to include("Image can't be blank")
+      end
+      it '職種名がないと投稿できない' do
+        @job.job_title = ''
+        @job.valid?
+        expect(@job.errors.full_messages).to include("Job title can't be blank")
+      end
+      it '店名 / 工房名がないと投稿できない' do
+        @job.workshop_name = ''
+        @job.valid?
+        expect(@job.errors.full_messages).to include("Workshop name can't be blank")
+      end
+      it '店名 / 工房の所在地が選択されている場合は出品できない' do
+        @job.origin_to_prefecture_id = 1
+        @job.valid?
+        expect(@job.errors.full_messages).to include("Origin to prefecture can't be blank")
+      end
+      it '仕事内容の説明がないと投稿できない' do
+        @job.description = ''
+        @job.valid?
+        expect(@job.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'userが紐づいていないと投稿できない' do
+        @job.user = nil
+        @job.valid?
+        expect(@job.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
