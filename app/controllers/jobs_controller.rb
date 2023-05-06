9bd0bdb825a6a0806039cef2ad092beb549a2e01
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-05-06 17:20:35 +0900

    記事詳細機能の実装

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_job, only: [:show]
 
   def index
     @jobs = Job.all.order('created_at DESC')
@@ -18,10 +19,16 @@ class JobsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def job_params
     params.require(:job).permit(:job_title, :workshop_name, :description, :origin_to_prefecture_id, :image).merge(user_id: current_user.id)
   end
   
+  def set_job
+    @job = Job.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "jobs#index"
-  resources :jobs, only: [:new, :create] do
+  resources :jobs, only: [:new, :create, :show] do
   end
 end
