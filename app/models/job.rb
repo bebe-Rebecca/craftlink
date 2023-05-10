c17aa9f2d789763d9dcee346f3f90bab8f31cb90
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-05-10 13:08:41 +0900

    Questionモデルにバリデーションを設定

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,6 +8,6 @@ class Job < ApplicationRecord
   validates :job_title,               presence: true
   validates :workshop_name,           presence: true
   validates :description,             presence: true
-  validates :image,                   presence: true                  
+  validates :image,                   presence: true
   validates :origin_to_prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  validates :name,    presence: true
+  validates :title,   presence: true
+  validates :content, presence: true
 end
